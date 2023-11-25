a326e6e34ff93d75d5713889b3a275462ac1569e
Ayat <ayatbaeshev@gmail.com>
2023-11-26 00:33:14 +0100

    created screen ship and added possibility for ship to move and fire bullets

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    def __init__(self):
+        #SCreen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 139)
+        #ship settings
+        self.ship_speed = 5
+        #bullet settings
+        self.bullet_speed = 8.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (253, 255, 0)
